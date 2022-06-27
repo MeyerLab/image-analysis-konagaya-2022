4b3ed94a865696c0275c5b8b1a10cac5f072bec8
Yumi-Ida <92470624+Yumi-Ida@users.noreply.github.com>
2022-06-27 00:23:25 -0400

    Add files via upload

---
+++ Tracking_code/Tracking_dependencies/Analysis/print_pdf.m
@@ -0,0 +1,9 @@
+function print_pdf(name)
+
+set(gcf,'Units','Inches');
+pos = get(gcf,'Position');
+set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)]);
+print(name,'-dpdf');
+
+end
+
